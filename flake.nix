ab18bfa984d746d77c66d5bd118a2e7801f588b7
jopejoe1 <34899572+jopejoe1@users.noreply.github.com>
2024-11-23 23:38:29 +0100

    use flake exposed systems

--- flake.nix
+++ flake.nix
@@ -77,12 +77,7 @@
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
       imports = [ ./flake-modules ];
     };
